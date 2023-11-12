1455daa11f2c6888b0b89e6cec0c7233fccc55e6
Yasdormiani <146694388+Yasdormiani@users.noreply.github.com>
2023-11-12 16:37:57 +0330

    Add files via upload

---
+++ sketch_Show_name_seriaalMonitor.ino
@@ -0,0 +1,13 @@
+int num;
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  if (Serial.available()) {
+    String name = Serial.readString();
+    Serial.print("recieved name:");
+    Serial.println(name);
+  }
+}
